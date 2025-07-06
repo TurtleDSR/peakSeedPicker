70b2e6c61aaea03d9d90dda4c2e0705b0e38dd92
TurtleDSR <Turtleman42007@gmail.com>
2025-07-05 21:53:03 -0400

    added ui to show what level players are on

--- com/turtledsr/peakseedpicker/Plugin.cs
+++ com/turtledsr/peakseedpicker/Plugin.cs
@@ -1,7 +1,10 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
+using BepInEx.Bootstrap;
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
+using UnityEngine;
 using Zorro.Core.Editor;
 
 namespace peakseedpicker;
@@ -9,9 +12,9 @@ namespace peakseedpicker;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin {
   internal static new ManualLogSource Logger;
-  
   public static ConfigEntry<int> levelNumConfig;
   public static int levelNum; //0 to 13
+  public static Vector2 overlayPos = new Vector2(1710, 10);
 
   private void Awake() {
     Logger = base.Logger;
@@ -23,6 +26,23 @@ public class Plugin : BaseUnityPlugin {
     harmony.PatchAll();
 
     Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+  }
+  
+  private void Start() {
+    if(Chainloader.PluginInfos.ContainsKey("com.turtledsr.peaktimer")) {
+      Logger.LogInfo("PeakTimer Found. Moving UI down");
+      overlayPos.y = 40;
+    }
+  }
+
+  private void OnGUI() {
+    GUIStyle style = new() {
+      fontSize = 30,
+      fontStyle = FontStyle.Bold,
+    };
+    style.normal.textColor = Color.white;
+
+    GUI.Label(new Rect(overlayPos.x, overlayPos.y, 200, 30),(levelNum >= 0 && levelNum <= 13) ? $"level_{levelNum}" : "Daily", style);
   }
 
   [HarmonyPatch(typeof(MapBaker), "GetLevel")]
